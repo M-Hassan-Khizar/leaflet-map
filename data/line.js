bee4a7dc173d6857e5f7a05a457a649c6cb75583
m-hassan-khizar <mhassankhizr@gmail.com>
2024-02-05 22:56:35 +0500

    add geojson and js

---
+++ data/line.js
@@ -0,0 +1,18 @@
+var lineJson = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [74.3587, 31.5204],   // Lahore
+            [72.8258, 35.2345],   // Lahore
+            [73.0551, 33.6844]     // Lahore (closing the loop)
+          ]
+        }
+      }
+    ]
+  };
+  
---
+++ data/point.js
@@ -0,0 +1,12 @@
+var pointJson = {
+    type: "FeatureCollection",
+    features: [
+      { type: "Feature", properties: {}, geometry: { type: "Point", coordinates: [73.0551, 33.6844] } }, // Islamabad
+      { type: "Feature", properties: {}, geometry: { type: "Point", coordinates: [67.0011, 24.8607] } }, // Karachi
+      { type: "Feature", properties: {}, geometry: { type: "Point", coordinates: [71.5249, 32.5880] } }, // Lahore
+      { type: "Feature", properties: {}, geometry: { type: "Point", coordinates: [72.8258, 35.2345] } }, // Peshawar
+      { type: "Feature", properties: {}, geometry: { type: "Point", coordinates: [74.3587, 31.5204] } }, // Multan
+      { type: "Feature", properties: {}, geometry: { type: "Point", coordinates: [74.3587, 31.5204] } }  // Quetta
+    ]
+  };
+  
